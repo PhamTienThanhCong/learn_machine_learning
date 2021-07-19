a0e0e46ecfd6fbe3aa61a6acbcf4ef51408f4f7e
cong <cong.pttc@gmail.com>
2021-07-19 15:06:58 +0700

    eror

--- linear_regression/home_work/TinhGiaNha.py
+++ linear_regression/home_work/TinhGiaNha.py
@@ -1,27 +1,18 @@
 from numpy.core.records import array
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 data = np.loadtxt('linear_regression/home_work/data.txt', delimiter = ',')
 x = data[:,0].reshape(-1, 1)
 y = data[:,1].reshape(-1, 1)
 
-learning_rate = 0.1
-w = [1.2,2.3,321.2]
-for j in range(1,1000):
-    cost_a = 0
-    cost_b = 0
-    cost_c = 0
-    for i in range(len(x)):
-        cost = w[0]*x[i]*x[i] + w[1]*x[i] + w[2]
-        cost_a += cost*x[i]*x[i]
-        cost_b += cost*x[i]
-        cost_c += cost
-    w[0] -= cost_a*learning_rate
-    w[1] -= cost_b*learning_rate
-    w[2] -= cost_c*learning_rate
+lrg = LinearRegression()
+lrg.fit(x,y)
 
-print(w)
-# plt.plot(x,y,'go')
-# plt.show()
+y_new = lrg.predict(x)
 
+plt.plot(x,y,'go')
+plt.plot(x,y_new,'yo')
+
+plt.show()
